d104b5282e3bad20b21c2043d4ba6bc10ad9cd6f
Bstev96R <bstev96r@gmail.com>
2019-11-09 17:35:34 -0600

    carrusel

--- SUGANIC/src/app/app.module.ts
+++ SUGANIC/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarruselComponent } from './Carrusel/carrusel/carrusel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarruselComponent
   ],
   imports: [
     BrowserModule,
