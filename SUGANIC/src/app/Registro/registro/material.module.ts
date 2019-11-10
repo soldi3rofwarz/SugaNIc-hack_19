da3489cfb90530f784c8dde66db53e9f13a38bd3
Berflo26 <b.valleflores@gmail.com>
2019-11-09 22:12:11 -0600

    nuevas modificaciones registro

--- SUGANIC/src/app/Registro/registro/material.module.ts
+++ SUGANIC/src/app/Registro/registro/material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule, } from '@angular/material';
 
 @NgModule({
   declarations: [],
