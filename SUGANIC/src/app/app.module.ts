c8aa1b6ea9cd23498fc4ed05de3c64b3316687ab
Bstev96R <bstev96r@gmail.com>
2019-11-09 23:27:23 -0600

    2da modificación al condigo

--- SUGANIC/src/app/app.module.ts
+++ SUGANIC/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
 
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CarruselComponent } from './Carrusel/carrusel/carrusel.component';
+
 import { RegistroComponent } from './Registro/registro/registro.component';
 import { LoginComponent } from './Login/login/login.component';
 import { NavbarComponent } from './Navbar/navbar/navbar.component';
 import { FooterComponent } from './Footer/footer/footer.component';
 import { CuerpoComponent } from './Cuerpo/cuerpo/cuerpo.component';
+import{NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarruselComponent } from './Carrusel/carrusel/carrusel.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -21,12 +23,12 @@ import { CuerpoComponent } from './Cuerpo/cuerpo/cuerpo.component';
     NavbarComponent,
     FooterComponent,
     CuerpoComponent,
-    MatInputModule
+    BrowserAnimationsModule
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    
+    NgbModule
     
   ],
   providers: [],
