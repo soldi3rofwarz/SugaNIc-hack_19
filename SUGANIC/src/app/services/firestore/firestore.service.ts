7d6aa9fbf26ac66265e9b2ef6e4a0185759eb081
Bstev96R <bstev96r@gmail.com>
2019-11-10 04:34:33 -0600

    funciones para el servicio

--- SUGANIC/src/app/services/firestore/firestore.service.ts
+++ SUGANIC/src/app/services/firestore/firestore.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
+
+  //crear nuevo ganado
+  public createCat(data: {nombre: string, url: string}) {
+    return this.firestore.collection('cats').add(data);
+  }
+
+  //Obtener todo los semovientes
+  public getCats() {
+    return this.firestore.collection('cats').snapshotChanges();
+  }
+
+
+  //Actualiza ganado
+  public updateCat(documentId: string, data: any) {
+    return this.firestore.collection('cats').doc(documentId).set(data);
+  }
 }
