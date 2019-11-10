b08fa283ee41e59e901c756ae5a7730845b81b20
Berflo26 <b.valleflores@gmail.com>
2019-11-09 18:24:05 -0600

    Instalación de componentes

--- SUGANIC/src/app/app.module.ts
+++ SUGANIC/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CarruselComponent } from './Carrusel/carrusel/carrusel.component';
+import { RegistroComponent } from './Registro/registro/registro.component';
+import { LoginComponent } from './Login/login/login.component';
+import { NavbarComponent } from './Navbar/navbar/navbar.component';
+import { FooterComponent } from './Footer/footer/footer.component';
+import { CuerpoComponent } from './Cuerpo/cuerpo/cuerpo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarruselComponent
+    CarruselComponent,
+    RegistroComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    CuerpoComponent
   ],
   imports: [
     BrowserModule,
