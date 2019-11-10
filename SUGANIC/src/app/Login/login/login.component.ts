a48726549c8759dfd5ca0ac3f1fe56bf68703315
Berflo26 <b.valleflores@gmail.com>
2019-11-10 05:22:53 -0600

    login

--- SUGANIC/src/app/Login/login/login.component.ts
+++ SUGANIC/src/app/Login/login/login.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-
-
 @Component({
-  selector: 'app-login',
+  selector: 'loginuser',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
+  
 })
 export class LoginComponent implements OnInit {
 
